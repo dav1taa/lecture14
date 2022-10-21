cbb324d2fffa431c0795d6086910e05c0c6c7d9b
dav1taa <nebieridze.dato12@gmail.com>
2022-10-21 21:34:40 +0400

    lecture14

---
+++ script2.js
@@ -0,0 +1,22 @@
+
+// function ფუნქციის სახელი ([პარამეტრი]) {
+//     ინსტურქცია
+// }
+
+
+function display () {
+    document.write("ეს არის ჯს პირველი ფუნქცია")
+}
+display()
+
+var display = function () {
+    document.write("ეს არის ჯს პირველი ფუნქცია")
+}
+display()
+
+
+
+
+
+
+
