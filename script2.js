8f92c5572c7f5f9306e8e4a49ea03cc9cf96a62e
dav1taa <nebieridze.dato12@gmail.com>
2022-10-21 22:57:18 +0400

    lecture14

--- script2.js
+++ script2.js
@@ -4,19 +4,36 @@
 // }
 
 
-function display () {
-    document.write("ეს არის ჯს პირველი ფუნქცია")
-}
-display()
+// function display () {
+//     document.write("ეს არის ჯს პირველი ფუნქცია")
+// }
+// display()
 
-var display = function () {
-    document.write("ეს არის ჯს პირველი ფუნქცია")
-}
-display()
-
-
+// var display = function () {
+//     document.write("ეს არის ჯს პირველი ფუნქცია")
+// }
+// display()
 
 
 
+// function display() {
+//     var z = 1 ;
+//     for (var i=0; i < arguments.length; i++)
+//         z *= arguments [i];
+//         document.write(z);
+// }
+// display (6,4,5)
 
 
+//  function getFactorial (n) {
+//     if (n===1) {
+//         return 1;
+//     }
+//     else {
+//         return n * getFactorial (n-1);
+
+//     }
+//  }
+
+//  var result = getFactorial(4);
+//  console.log(result)
